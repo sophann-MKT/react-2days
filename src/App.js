828a0b992d98c61c2abff3e6bd89c8e9bda4c6d3
sophann <sophann.morakot@gmail.com>
2023-11-17 16:59:11 +0700

    do todo-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import React, { useState } from "react";
 import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleInputChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const handleTodoSubmit = () => {
+    if (todo.trim() !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
   return (
     <>
-      <h1>Hello react Js</h1>
+      <h1>Todo</h1>
+      <input
+        type="text"
+        placeholder="Enter Your Todo..."
+        value={todo}
+        onChange={handleInputChange}
+      />
+      <TodoList todos={todos} />
+      <button onClick={handleTodoSubmit}>Submit</button>
     </>
   );
 }
